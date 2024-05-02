59b91f07a02c703254b78c21d773549b4b7e494e
SanketThawari-Dev <sanketthawarimb@gmail.com>
2024-05-02 20:13:56 +0530

    project added

---
+++ src/main/java/com/MainApp/Controller/Launch1.java
@@ -0,0 +1,37 @@
+package com.MainApp.Controller;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.MainApp.ConnectionFactory.ConnectionFactory;
+import com.MainApp.Dao.UserDao;
+@WebServlet("/signup")
+public class Launch1 extends HttpServlet{
+
+	Connection con = ConnectionFactory.getCon();
+	UserDao uDao = new UserDao();
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = req.getParameter("uid");
+		String pass = req.getParameter("upass");
+		
+		String res = uDao.insert(con, id, pass);
+		if(res.equals("inserted"))
+		{
+			resp.sendRedirect("login.jsp");
+		}
+		else
+		{
+			req.getSession().setAttribute("msg", "user exists");
+			resp.sendRedirect("signup.jsp");
+		}
+	}
+}
+
